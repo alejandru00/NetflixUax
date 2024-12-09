6fee91b50850ee81e25d5d9e6eedbfb2e9dbe1b5
alejandru00 <alejandrolliber2004@gmail.com>
2024-12-09 17:09:34 +0100

    mas

--- netflix/authentication/urls.py
+++ netflix/authentication/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import signup, login_view, logout_view
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('signup/', signup, name='signup'),
-    path('login/', login_view, name='login'),
-    path('logout/', logout_view, name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='authentication/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
